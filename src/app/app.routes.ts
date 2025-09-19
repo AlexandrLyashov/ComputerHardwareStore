8b9d1415cdc61869d4ff5720dd312f357ced4dff
Alexandr Lyashov <faler1337@gmail.com>
2025-09-19 14:28:37 +0300

    before angular update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./features/products/pages/product-list/product-list.component')
+        .then(m => m.ProductList),
+  },
+  {
+    path: 'products/:id',
+    loadComponent: () =>
+      import('./features/products/pages/product-detail/product-detail.component')
+        .then(m => m.ProductDetail),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./features/cart/pages/cart-page/cart-page.component')
+        .then(m => m.CartPageComponent),
+  },
+  {
+    path: 'checkout',
+    loadComponent: () =>
+      import('./features/checkout/pages/checkout-page/checkout-page.component')
+        .then(m => m.CheckoutPage),
+  },
+
+  { path: '**', redirectTo: 'products' },
+];
